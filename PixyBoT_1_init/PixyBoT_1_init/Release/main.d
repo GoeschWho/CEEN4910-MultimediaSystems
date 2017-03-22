cff5bffa010a29f15cb52af783149c8c2ca70f2e
Megan Bird <mlgoesch11@gmail.com>
2017-03-22 13:32:58 -0500

    1_init
    Basic test of Pixy to bot communication.
    Displays data on LCD.
    Commented out movement in BBC skelly.

---
+++ PixyBoT_1_init/PixyBoT_1_init/Release/main.d
@@ -0,0 +1,139 @@
+main.d main.o: .././main.c \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/capi324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/utils324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/sys324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/psxc324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom324p.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/spi324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/tmrsrvc324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/isr324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/lcd324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/tiny324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/spkr324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/led324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/mega324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/step324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/swatch324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/usonic324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/uart324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/adc324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/spiflash324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/ti324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/i2c324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/cmucam324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/pixy324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/bbtk324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/cbot324v221.h \
+ C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/capi324v221.h
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/capi324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/utils324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/sys324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/psxc324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom324p.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/spi324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/tmrsrvc324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/isr324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/lcd324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/tiny324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/spkr324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/led324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/mega324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/step324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/swatch324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/usonic324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/uart324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/adc324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/spiflash324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/ti324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/i2c324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/cmucam324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/pixy324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/bbtk324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/cbot324v221.h:
+
+C:\Users\megan\Google\ Drive\College\S6\ -\ Spring\ 2017\Mobile\ Robotics\Lab\Library\lib-includes/capi324v221.h:
